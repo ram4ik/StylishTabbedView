b7739ac556b43e39c5db60fcf3aa0cf18035fb70
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-16 07:09:34 +0300

    init

--- StylishTabbedView/ContentView.swift
+++ StylishTabbedView/ContentView.swift
@@ -10,12 +10,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
+        TabView {
+            page1().tabItem {
+                VStack {
+                    Image(systemName: "house.fill")
+                    Text("Home").fontWeight(.light)
+                }
+            }
+            page2().tabItem {
+                VStack {
+                    Image(systemName: "flame.fill")
+                    Text("Flame").fontWeight(.light)
+                }
+            }
+            page3().tabItem {
+                VStack {
+                    Image(systemName: "paperplane.fill")
+                    Text("Plane").fontWeight(.light)
+                }
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct page1: View {
+    var body: some View {
+        Text("Hello from page 1").fontWeight(.heavy)
+    }
+}
+
+struct page2: View {
+    var body: some View {
+        Text("Hello from page 2").fontWeight(.heavy)
+    }
+}
+
+struct page3: View {
+    var body: some View {
+        Text("Hello from page 3").fontWeight(.heavy)
     }
 }
